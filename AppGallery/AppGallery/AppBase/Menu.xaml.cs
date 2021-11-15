a0f97967cbac51a35ae8177621d609eb32c22a20
LucasCruz <71300702+Lucas-PCruz@users.noreply.github.com>
2021-11-15 19:23:05 -0300

    commit com componentes de layout.

--- AppGallery/AppGallery/AppBase/Menu.xaml.cs
+++ AppGallery/AppGallery/AppBase/Menu.xaml.cs
@@ -53,5 +53,35 @@ namespace AppGallery.AppBase
             ((MasterDetailPage)App.Current.MainPage).Detail = new XamarinForms.Paginas.Modal.Conteudo01();
             ((MasterDetailPage)App.Current.MainPage).IsPresented = false;
         }
+
+        private void AbrirStackLayout(object sender, EventArgs e)
+        {
+            ((MasterDetailPage)App.Current.MainPage).Detail = new XamarinForms.Leioutes.PilhaLayout.Pilha();
+            ((MasterDetailPage)App.Current.MainPage).IsPresented = false;
+        }
+
+        private void Abrirgrid(object sender, EventArgs e)
+        {
+            ((MasterDetailPage)App.Current.MainPage).Detail = new XamarinForms.Leioutes.GradeLeiaute.Grade();
+            ((MasterDetailPage)App.Current.MainPage).IsPresented = false;
+        }
+
+        private void AbrirAbsolutLayout(object sender, EventArgs e)
+        {
+            ((MasterDetailPage)App.Current.MainPage).Detail = new XamarinForms.Leioutes.AbsolutoLeiaute.Absoluto();
+            ((MasterDetailPage)App.Current.MainPage).IsPresented = false;
+        }
+
+        private void AbrirRelativeLayout(object sender, EventArgs e)
+        {
+            ((MasterDetailPage)App.Current.MainPage).Detail = new XamarinForms.Leioutes.RelativoLayout.Relativo();
+            ((MasterDetailPage)App.Current.MainPage).IsPresented = false;
+        }
+
+        private void AbrirFlexLayout(object sender, EventArgs e)
+        {
+            ((MasterDetailPage)App.Current.MainPage).Detail = new XamarinForms.Leioutes.FlexivelLayout.Flexivel();
+            ((MasterDetailPage)App.Current.MainPage).IsPresented = false;
+        }
     }
 }
